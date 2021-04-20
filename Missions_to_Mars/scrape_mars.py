a89b4d895e44942772f776d67c9deea01f215405
osie78 <oscarsanguino@hotmail.com>
2021-04-19 22:36:09 -0500

    4/19 22:36

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,35 +8,39 @@ import pandas as pd
 
 
 
+def latest_news(): 
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
+    # NASA Mars News
 
-
-
-def head_par():
-    driver = webdriver.Chrome(ChromeDriverManager().install()
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     driver.get(url)
-    
+
     headlines=driver.page_source
     soup = bs(headlines)
 
     #pulling the last header 
     heads=soup.find_all("div", class_="content_title")[1].text
-    latest_header=heads
     
+    latest_header=heads
+        
     #scrapping the text paragraph associated to the first header
     paragraph=soup.find_all('div', class_='article_teaser_body')[0].text
 
-    text_list=['latest_header']=latest_header
-    text_list=['paragraph']=paragraph
+    text_dict={}
+    text_dict['headers'] = latest_header
+    text_dict['paragraph']=paragraph
+    
+    driver.close()
+    return text_dict
 
-    return text_list
 
 
-def photo():
-    # JPL Mars Space Images - Featured Image
-    driver = webdriver.Chrome(ChromeDriverManager().install()
-    #repeating the same process with a new url
+# JPL Mars Space Images - Featured Image
+
+def feat_image():
+    #repeting the same process with a new url
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     url2='https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     driver.get(url2)
 
@@ -46,27 +49,30 @@ def photo():
 
     # the class "headerimage is unique in the html file. Then, we'll get the source as "src".
     feat_image=soup.find("img", class_="headerimage").get('src')
+    
 
     #will construct the url by concatenating the two strings
     url2_dummy=url2.replace("index.html","")
-
-    # this is the final url for the featured image in the website.
-    final_img_url=url2_dummy+feat_image
     
-    return final_img_url
+    final_img_url=url2_dummy+feat_image
+    driver.close()
+
+    return final_img_url # this is the final url for the featured image in the website.
 
 
+# Mars Hemispheres
 
 def hemispheres():
-    # Mars Hemispheres
-    driver = webdriver.Chrome(ChromeDriverManager().install()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    #browsing the titles. The titles are all <h3>.
+#browsing the titles. The titles are all <h3>.
+
     url3='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     driver.get(url3)
 
     hemisphere_titles=driver.page_source
     soup = bs(hemisphere_titles)
+
 
     #extracting the 4 hemisphere titles
     c=[]
@@ -74,6 +80,7 @@ def hemispheres():
 
     for x in range(0,len(hem_names)):
         c.append(hem_names[x].text)
+
 
     #composing the links that take to the individual hemisphere page
     main_url='https://astrogeology.usgs.gov'
@@ -84,7 +91,7 @@ def hemispheres():
         urls4=main_url+acc_link
         comp_url.append(urls4)
 
-    # comp_url contains the links for each hemisphere
+    # comp_url This list contains the links for each hemisphere
     
 
     # Scrapping the links in each individual hemisphere website where the high resolution picture can be downloaded
@@ -98,7 +105,6 @@ def hemispheres():
         enh_pic_url.append(full_image)
     
 
-
     #building the requested dictionary
 
     final_url_name = []
@@ -110,27 +116,19 @@ def hemispheres():
         dict["img_url"] = enh_pic_url[i]
         
         final_url_name.append(dict)
-        
-    return final_url_name
+            
+         
+    return(final_url_name)
 
-
-def all():
-    all_dictionary={
-        'headers_paragraphs':head_par(),
-        'feature_image':photo(),
-        'hemispheres_pictures_url':hemispheres()
-    }
-    return all_dictionary    
-
-def all_to_html():
-    conv_to_df=all()
-    df=pd.DataFrame(conv_to_df)
-
-    return df.to_html(classes='table table-striped')
+def scrape():
+    scrape_dict={'news':latest_news,
+    'featured_image'=feat_image,
+    'hemispheres'=final_url_name}
 
 
 
-    
+
+
 
 
 
